cf4826323022b31097e73d3ff6dbe8cea7d4050a
wendyasada12 <wendyasada4@gmail.com>
2022-03-02 12:54:18 +0800

    init

---
+++ app/src/main/java/com/ubaya/a160419062_advweek4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.a160419062_advweek4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
